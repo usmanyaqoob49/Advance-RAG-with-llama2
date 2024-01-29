3e78deed033089515eda6d2dc3dac1fb00332608
Usman Yaqoob <68185452+usmanyaqoob49@users.noreply.github.com>
2024-01-29 14:19:25 +0500

    Add files via upload

---
+++ embed_4.py
@@ -0,0 +1,5 @@
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+
+def embedding_function():
+    embedding_function = SentenceTransformerEmbeddingFunction()
+    return embedding_function
